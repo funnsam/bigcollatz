06fd76880a4bde4dabe16420c8c455ff9f2a3505
funnsam <funnsam8@gmail.com>
2025-11-09 15:17:50 +0000

    1 tier divide and conqer

--- src/solver.zig
+++ src/solver.zig
@@ -1,3 +1,5 @@
+// split off a part of the bigint to solve, and bulk update it when the divided part is about to overflow by keeping track of operations and update by (mul*num + add) >> rsh
+
 const std = @import("std");
 
 const ubig = @import("bigcollatz").ubig;
@@ -12,11 +14,41 @@ pub fn solve(_num: ubig) !void {
     num.rsh(prefactor_count);
 
     while (!std.mem.eql(ubig.Digit, num.digits.items, &.{1})) {
-        counters.odd += 1;
-        try num.mulAddSd(3, 1);
+        const update = solveSmall(num.digits.items[0] & small_chunk_mask);
 
-        const rep = num.ctz();
-        counters.even += rep;
-        num.rsh(rep);
+        try num.mulAddSd(update.mul, update.add);
+
+        const ctz = num.ctz();
+        num.rsh(ctz);
+        counters.even += ctz;
     }
+}
+
+const small_chunk_size = @typeInfo(usize).int.bits / 2;
+const small_chunk_mask = (1 << small_chunk_size) - 1;
+
+
+const Update = struct {
+    mul: usize = 1,
+    add: usize = 0,
+    rsh: u6 = 0,
+};
+
+fn solveSmall(_num: usize) Update {
+    var num: usize = _num;
+    var update: Update = .{};
+
+    while (num != 1 and update.rsh < small_chunk_size) {
+        num = num * 3 + 1;
+
+        update.mul *= 3;
+        update.add = update.add * 3 + (@as(usize, 1) << update.rsh);
+        counters.odd += 1;
+
+        const trail = @min(small_chunk_size - update.rsh, @ctz(num));
+        num >>= @intCast(trail);
+        update.rsh += @intCast(trail);
+    }
+
+    return update;
 }
