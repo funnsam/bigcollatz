83ad239abe34a8d9e5ea1ced55501453a6acec99
funnsam <funnsam8@gmail.com>
2025-11-10 07:59:03 +0000

    refac 2

--- src/solver.zig
+++ src/solver.zig
@@ -1,4 +1,7 @@
-// split off a part of the bigint to solve, and bulk update it when the divided part is about to overflow by keeping track of operations and update by (mul*num + add) >> rsh
+// split off a part of the bigint to solve, and bulk update it when the divided part is about to
+// overflow by keeping track of operations and update by (mul*num + add) >> rsh
+//
+// this saves a lot(!!) of processing time by having to update the bigint less
 
 const std = @import("std");
 
@@ -14,24 +17,25 @@ pub fn solve(_num: ubig) !void {
     num.rsh(prefactor_count);
 
     while (!std.mem.eql(ubig.Digit, num.digits.items, &.{1})) {
-        const update = solveSmall(num.digits.items[0] & small_chunk_mask);
-
-        try num.mulAddSd(update.mul, update.add);
-
-        const ctz = num.ctz();
-        num.rsh(ctz);
-        counters.even += ctz;
+        try solveSmall(num.digits.items[0] & small_chunk_mask).apply(&num);
     }
 }
 
 const small_chunk_size = @typeInfo(usize).int.bits / 2;
 const small_chunk_mask = (1 << small_chunk_size) - 1;
 
-
 const Update = struct {
     mul: usize = 1,
     add: usize = 0,
     rsh: u6 = 0,
+
+    fn apply(self: @This(), num: *ubig) !void {
+        try num.mulAddSd(self.mul, self.add);
+
+        const ctz = num.ctz();
+        num.rsh(ctz);
+        counters.even += ctz;
+    }
 };
 
 fn solveSmall(_num: usize) Update {
